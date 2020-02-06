d803ca2d2799ee78da87fd123da8abe832556b37
sune <sune.bro@ka-net.dk>
2020-02-06 11:59:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mvvm_demo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.mvvm_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText editText;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText = (EditText)findViewById(R.id.editText);
+                textView = (TextView)findViewById(R.id.TextView);
+                textView.setText("hej" +editText.getText().toString()+"!");
+            }
+        });
+    }
+}
